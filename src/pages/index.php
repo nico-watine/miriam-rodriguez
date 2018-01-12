d3c5cf42d85d002c28d9d4951d55b0c514a3c86d
Nico Watine <nico@studioncreations.com>
2018-01-12 09:51:09 -0600

    Back to basics, start small-up

--- src/pages/index.php
+++ src/pages/index.php
@@ -63,8 +63,8 @@ body-class: index
 		</div>
 	</div>
 	<div class="row">
-		<div class="medium-6 columns text-right">
-			<h3 class="h2 serif" style="text-shadow: 1px 2px 0 #fff;">Armonia,<br> Elegancia y<br> Distinción</h3>
+		<div class="medium-6 columns">
+			<h3 class="h2 serif" style="text-shadow: 1px 2px 0 #fff;">Armonia, Elegancia y Distinción</h3>
 			<img src="/img/bio/miriam-rodriguez.jpg" alt="">
 		</div>
 		<div class="medium-6 columns">
