014af4c87c31b5daa6fb092424a1fef377533305
Nico Watine <nico@studioncreations.com>
2018-01-12 05:31:22 -0600

    Convert the address h4 to address tag

--- src/pages/index.php
+++ src/pages/index.php
@@ -68,8 +68,8 @@ body-class: index
 			<h3 id="name"><span class="serif">Miriam Rodríguez,</span> <small>Alta Costura</small></h3>
 			<h4>@realmiriamrodriguez</h4>
 			<br>
-			<h4>Ave 9, calle 66A Nro 66A-17 <br>
-			Maracaibo, Venezuela 04002</h4>
+			<address class="h4">Ave 9, calle 66A Nro 66A-17 <br>
+			Maracaibo, Venezuela 04002</address>
 			<br>
 			<h4>(0261) 741.15.82 - (0414) 364.63.12</h4>
 			<br>
