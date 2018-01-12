c24bdeb2b750d878e80d5f594a8bb34ed8b826d0
Nico Watine <nico@studioncreations.com>
2018-01-12 03:38:10 -0600

    Specify strong sections of her instagram name

--- src/pages/index.php
+++ src/pages/index.php
@@ -52,7 +52,7 @@ body-class: index
 	<div class="row padding-Y expanded collapse sectionHeading">
 		<div class="columns text-center content">
 			<br>
-			<h2>@REALMIRIAMRODRIGUEZ</h2>
+			<h2><strong>@</strong>REAL<strong>MIRIAM</strong>RODRIGUEZ</h2>
 			<br>
 		</div>
 	</div>
