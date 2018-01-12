1359b2efd417f083343a8600d62f25f4c5bac503
Nico Watine <nico@studioncreations.com>
2018-01-11 22:11:03 -0600

    Add h2 entry

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,6 +9,11 @@ body-id: index
 body-class: index
 ---
 <section class="bridals">
+	<div class="row">
+		<div class="columns">
+			<h2 class="text-center"><strong>BRIDALS</strong></h2>
+		</div>
+	</div>
 	<div class="row collapse">
 		<div class="medium-6 large-3 columns"><img src="/img/bridals/bridals-1.jpg" alt=""></div>
 		<div class="medium-6 large-3 columns"><img src="/img/bridals/bridals-2.jpg" alt=""></div>
