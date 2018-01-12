b631427628c53da97b3395dc91789d654c9b095d
Nico Watine <nico@studioncreations.com>
2018-01-12 04:26:48 -0600

    Moved the curator.io javascript from layout src

--- src/pages/index.php
+++ src/pages/index.php
@@ -59,3 +59,13 @@ body-class: index
 </section>
 
 <div id="curator-feed"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div>
+
+<!-- The Javascript can be moved to the end of the html page before the </body> tag -->
+<script type="text/javascript">
+// curator-feed
+(function(){
+    var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
+    i.src = "https://cdn.curator.io/published/a82c8ade-f8fd-49f6-b452-f5aaae18.js";
+    e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+})();
+</script>
