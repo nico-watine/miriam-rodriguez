0d706a39da0b81b97a149ff0d15407428217fe13
Nico Watine <nico@studioncreations.com>
2018-01-12 04:57:45 -0600

    Move hidden curator.io snippet from src to PROD layout

--- src/pages/index.php
+++ src/pages/index.php
@@ -60,13 +60,3 @@ body-class: index
 	<br>
 	<br>
 </section>
-
-
-<!-- Curator.io Instagram feed javascript snippet -->
-<!-- <script type="text/javascript">
-(function(){
-	var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
-	i.src = "https://cdn.curator.io/published/a82c8ade-f8fd-49f6-b452-f5aaae18.js";
-	e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
-})();
-</script> -->
