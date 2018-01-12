f33eba0e05185f9a3dafd37dc62825135d2f761e
Nico Watine <nico@studioncreations.com>
2018-01-12 08:00:01 -0600

    Move inline styles to pre-existing classes

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,8 +8,8 @@ og-image:
 body-id: index
 body-class: index
 ---
-<section class="splash text-center" style="position: relative;">
-	<div class="logoWrapper" style="position: absolute; width: 100%; height: 100%; display: flex; justify-content: center;">
+<section class="splash text-center relative">
+	<div class="logoWrapper flex" style="position: absolute; width: 100%; height: 100%; justify-content: center;">
 		<img style="width: 440px; opacity: .6;" src="/img/splash/logo.svg" alt="">
 	</div>
 	<div class="row expanded collapse">
