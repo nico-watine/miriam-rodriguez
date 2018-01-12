e7d9111dd45d7912ab1f79e4ec7c55d507464100
Nico Watine <nico@studioncreations.com>
2018-01-12 10:34:50 -0600

    Specify display for adjective intro

--- src/pages/index.php
+++ src/pages/index.php
@@ -64,7 +64,7 @@ body-class: index
 	</div>
 	<div class="row">
 		<div class="medium-6 columns medium-text-right padding-0">
-			<h3 class="padding-X h2 serif" style="text-shadow: 1px 2px 0 #fff;">Armonia, Elegancia y Distinción</h3>
+			<h3 class="padding-X h2 serif" style="text-shadow: 1px 2px 0 #fff;"><span class="block">Armonia,</span> <span class="block">Elegancia y</span> <span class="block">Distinción</span></h3>
 			<br>
 			<img class="block" src="/img/bio/miriam-rodriguez.jpg" alt="">
 			<br>
