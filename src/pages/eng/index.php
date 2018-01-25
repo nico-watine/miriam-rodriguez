48db2cf477378bfea11bbe9aa88399e8e6000798
Nico Watine <nico@studioncreations.com>
2018-01-25 10:18:46 -0600

    Quick copy/paste of index into eng

---
+++ src/pages/eng/index.php
@@ -0,0 +1,127 @@
+---
+url: /
+title: Miriam Rodríguez, Alta Costura
+description: Bridal gowns and prêt-à-porter evening wear by Venezuelan couturier Miriam Rodríguez
+og-title: Miriam Rodríguez, Haute Couture Bridals
+og-description: Bridal gowns and prêt-à-porter evening wear by Venezuelan couturier Miriam Rodríguez
+og-image: /img/assets/og-images/miriam-rodriguez-bridals.png
+body-id: index
+body-class: index
+---
+<section class="splash text-center relative">
+	<div data-magellan class="iconWrapper absolute show-for-medium">
+		<a href="#contact"><img src="/img/assets/contact.svg" alt="Contact Icon"></a>
+	</div>
+	<div class="masthead flex absolute">
+		<div class="prose absolute">
+			<h1 class="serif">Miriam Rodríguez</h1>
+			<nav>
+				<ul class="menu inline-block">
+					<li data-threshold="0" data-magellan><a class="h3" href="#bridals">Bridal</a></li>
+					<li class="h3"><strong>|</strong></li>
+					<li data-threshold="0" data-magellan><a class="h3" href="#bio">Bio</a></li>
+				</ul>
+			</nav>
+			<div data-magellan class="iconWrapper show-for-small-only">
+				<a href="#contact"><img src="/img/assets/contact.svg" alt="Contact Icon"></a>
+			</div>
+		</div>
+		<div class="logoWrapper">
+			<img src="/img/splash/logo.svg" alt="Miriam Rodriguez Logo">
+		</div>
+	</div>
+	<div class="row expanded collapse">
+		<div class="medium-6 columns">
+			<img class="backgroundSplash" src="/img/splash/splash-1.jpg" alt="">
+		</div>
+		<div class="medium-6 columns show-for-medium">
+			<img class="backgroundSplash" src="/img/splash/splash-2.jpg" alt="">
+		</div>
+	</div>
+</section>
+
+<section id="bridals" class="bridals" data-magellan-target="bridals">
+	<div class="row padding-Y expanded collapse sectionHeading">
+		<div class="columns text-center content">
+			<br>
+			<h2 class="white"><strong>Bridals</strong></h2>
+			<br>
+		</div>
+	</div>
+	<div class="row collapse imageGallery text-center">
+		<div class="medium-4 large-3 columns"><img src="/img/bridals/bridals-7.jpg" alt=""></div>
+		<div class="medium-4 large-3 columns"><img src="/img/bridals/bridals-3.jpg" alt=""></div>
+		<div class="medium-4 large-3 columns"><img src="/img/bridals/bridals-2.jpg" alt=""></div>
+		<div class="medium-4 large-3 columns"><img src="/img/bridals/bridals-4.jpg" alt=""></div>
+		<div class="medium-4 large-3 columns"><img src="/img/bridals/bridals-5.jpg" alt=""></div>
+		<div class="medium-4 large-3 columns"><img src="/img/bridals/bridals-6.jpg" alt=""></div>
+		<div class="medium-6 columns hide-for-large"><img src="/img/bridals/miriam-rodriguez-bridal-shot-2.jpg" alt=""></div>
+		<div class="medium-6 columns hide-for-large"><img src="/img/bridals/miriam-rodriguez-bridal-shot-1.jpg" alt=""></div>
+		<div class="show-for-large large-3 columns"><img src="/img/bridals/bridals-8.jpg" alt=""></div>
+		<div class="show-for-large large-3 columns"><img src="/img/bridals/bridals-1.jpg" alt=""></div>
+	</div>
+</section>
+
+<section id="bio" class="bio">
+	<div class="row padding-Y expanded collapse sectionHeading">
+		<div class="columns text-center content">
+			<br>
+			<h2><strong>Bio</strong></h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="medium-6 columns medium-text-right padding-0">
+			<h3 class="padding-X h2 serif" style="text-shadow: 1px 2px 0 #fff;">Armonia, Elegancia y Distinción</h3>
+			<br>
+			<div class="pictureWrapper relative text-center">
+				<h3 class="serif white absolute margin-0 padding-Y">Miriam Rodríguez</h3>
+				<picture class="block">
+					<source srcset="/img/bio/miriam-rodriguez.jpg" media="(min-width: 880px)">
+					<source srcset="/img/bio/medium/miriam-rodriguez.jpg" media="(min-width: 640px)">
+					<img src="/img/bio/miriam-rodriguez.jpg" alt="…">
+				</picture>
+			</div>
+			<br>
+		</div>
+		<div class="medium-6 columns padding-XY prose">
+			<p class="intro">Adjetivos que definen claramente las propuestas de Miriam Rodríguez, siempre inspirada en la mujer latina y su cuerpo de curvas sinuosas.</p>
+			<span class="bar block"></span>
+			<br>
+			<p>Nacida en Santa Marta, Colombia, pero zuliana por eleccion, Miriam Rodríguez tiene 45 años vistiendo a las mujeres más elegantes de la sociedad venezolana.</p>
+			<p>Su estilo siempre clásico y atemporal marca la elegancia y destaca a quien lo luce.</p>
+		</div>
+	</div>
+</section>
+
+<section class="instagram">
+	<div class="row expanded collapse sectionHeading">
+		<div class="columns text-center content padding-Y">
+			<br>
+			<h2 class="white"><span class="inline-block"><strong>@</strong>real</span><span class="inline-block"><strong>Miriam</strong></span><span class="inline-block">Rodriguez</span></h2>
+			<br>
+		</div>
+	</div>
+	<!-- <div id="curator-feed"></div> -->
+	<br>
+	<br>
+</section>
+
+<section id="contact" class="contact">
+	<hr>
+	<div class="row text-center">
+		<div class="columns prose">
+			<h3 id="name"><span class="serif">Miriam Rodríguez,</span> <div class="inline-block"><small>Alta Costura</small></div></h3>
+			<a class="h4 block" href="https://www.instagram.com/realmiriamrodriguez/">@realmiriamrodriguez</a>
+			<br>
+			<address class="h4">Ave 9, calle 66A Nro 66A-17 <br>
+			Maracaibo, Venezuela 04002</address>
+			<br>
+			<h4>(0261) 741.15.82 - (0414) 364.63.12</h4>
+			<br>
+			<a class="h4 block" href="mailto:info@miriamrodriguez.com">info@miriamrodriguez.com</a>
+			<br>
+			<br>
+		</div>
+	</div>
+</section>
+
