42a9bdce62b796e3bb752b12228093eea0aff229
Nico Watine <nico@studioncreations.com>
2018-01-25 15:28:01 -0600

    Change html lang value to variable and set eng

--- src/pages/eng/index.php
+++ src/pages/eng/index.php
@@ -2,6 +2,7 @@
 url: /
 title: Miriam Rodríguez, High Fashion Bridals
 description: Bridal gowns and prêt-à-porter evening wear by Venezuelan couturier Miriam Rodríguez
+html-lang: en-US
 og-locale: en_US
 og-title: Miriam Rodríguez, High Fashion Bridals
 og-description: Bridal gowns and prêt-à-porter evening wear by Venezuelan couturier Miriam Rodríguez
