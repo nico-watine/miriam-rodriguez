1dbecb1e97dae6f7c158cf020937dd20bead8473
Nico Watine <nico@studioncreations.com>
2018-02-01 09:50:16 -0600

    Add all popup ignore classes

--- js/src/app.js
+++ js/src/app.js
@@ -1,16 +1,1 @@
 $(document).foundation();
-
-$(document).ready(function() {
-	
-	$('.gallery').each(function() { // the containers for all your galleries
-		$(this).magnificPopup({
-			delegate: 'a', // the selector for gallery item
-			type: 'image',
-			gallery: {
-			  enabled:true
-			}
-		});
-	});
-});
-
-new WOW().init();
