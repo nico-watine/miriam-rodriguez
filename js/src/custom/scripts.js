28961ae191d99b6f3f9786fa38d8f6d59f0a9658
Nico Watine <nico@studioncreations.com>
2018-02-01 09:38:19 -0600

    Add custom js into scripts.js

--- js/src/custom/scripts.js
+++ js/src/custom/scripts.js
@@ -2,3 +2,18 @@
 // 'scripts.js' is a placehold file for any other script files
 // 'scripts.js' is minified via codekit, not via 'npm run build'
 // when 'npm run build' is executed, 'scripts-min.js' is simply copied over and not processed in any way
+
+$(document).ready(function() {
+	
+	$('.gallery').each(function() { // the containers for all your galleries
+		$(this).magnificPopup({
+			delegate: 'a', // the selector for gallery item
+			type: 'image',
+			gallery: {
+			  enabled:true
+			}
+		});
+	});
+});
+
+new WOW().init();
